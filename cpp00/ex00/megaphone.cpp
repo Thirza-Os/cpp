73fd4c35d22fa7fb50d81ab3bb16edb75b2cc705
tosinga <tosinga@student.codam.com>
2022-10-06 12:17:55 +0200

    firs commit

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char *argv[])
+{
+	std::string	ret;
+	int		i;
+
+	if (argc > 1)
+	{
+		if (argv[1][0] == '\0')
+			return (0);
+		else if (argc == 1)
+			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		else
+		{
+			for (i = 1; i < argc; i++)
+			{
+				if (argv[i] == '\0')
+					i++;
+				ret = ret + argv[i];
+			}
+			std::transform(ret.begin(), ret.end(), ret.begin(), ::toupper);
+			std::cout << ret << std::endl;
+		}
+	}
+	return (0);
+}
