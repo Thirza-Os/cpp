c79bb84118635b8227c9a02929fdbdb3cce0a3ee
tosinga <tosinga@student.codam.com>
2023-01-31 19:36:01 +0100

    last

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -2,34 +2,35 @@
 
 #include <iostream>
 
-int main(void)
-{
-    Span sp = Span(102);
-    std::cout << sp.getSize() << std::endl;
-
-    for (int i = 0; i < 1; i++)
-        sp.addNumber(9);
-    sp.addNumber(20);
-    sp.addNumber(10);
-    std::cout << "Span size:        " << sp.getSize() << std::endl;
-    std::cout << "Shortest span:    " << sp.shortestSpan() << std::endl;
-    std::cout << "Longest span:     " << sp.longestSpan() << std::endl;
-
-    sp.addRange(1, 100);
-
-    sp.printRange();
-
-}
-
-// int main()
+// int main(void)
 // {
-// Span sp = Span(100000);
-// sp.addNumber(6);
-// sp.addNumber(3);
-// sp.addNumber(17);
-// sp.addNumber(9);
-// sp.addNumber(11);
-// std::cout << sp.shortestSpan() << std::endl;
-// // std::cout << sp.longestSpan() << std::endl;
-// return 0;
+//     Span sp = Span(200);
+//     std::cout << sp.getSize() << std::endl;
+
+//     for (int i = 0; i < 1; i++)
+//         sp.addNumber(9);
+//     sp.addNumber(20);
+//     sp.addNumber(10);
+//     std::cout << "Span size:        " << sp.getSize() << std::endl;
+//     std::cout << "Shortest span:    " << sp.shortestSpan() << std::endl;
+//     std::cout << "Longest span:     " << sp.longestSpan() << std::endl;
+
+//     sp.addRange(1, 100);
+
+//     sp.printRange();
+
 // }
+
+int main()
+{
+Span sp = Span(100000);
+sp.addNumber(-0);
+sp.addNumber(0);
+// sp.addNumber(17);
+// sp.addNumber(8);
+// sp.addNumber(11);
+std::cout << sp.shortestSpan() << std::endl;
+std::cout << sp.longestSpan() << std::endl;
+std::cout << (unsigned int)-INT_MIN + (unsigned int)INT_MAX << std::endl;
+return 0;
+}
