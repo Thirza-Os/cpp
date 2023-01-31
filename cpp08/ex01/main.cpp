b1e48b0b73716bcbfd5b146909f9f49fb6cc93d8
tosinga <tosinga@student.codam.com>
2023-01-31 17:28:02 +0100

    things

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,6 +1,35 @@
 #include "Span.hpp"
 
+#include <iostream>
+
 int main(void)
 {
-    Span(7) sp;
+    Span sp = Span(102);
+    std::cout << sp.getSize() << std::endl;
+
+    for (int i = 0; i < 1; i++)
+        sp.addNumber(9);
+    sp.addNumber(20);
+    sp.addNumber(10);
+    std::cout << "Span size:        " << sp.getSize() << std::endl;
+    std::cout << "Shortest span:    " << sp.shortestSpan() << std::endl;
+    std::cout << "Longest span:     " << sp.longestSpan() << std::endl;
+
+    sp.addRange(1, 100);
+
+    sp.printRange();
+
 }
+
+// int main()
+// {
+// Span sp = Span(100000);
+// sp.addNumber(6);
+// sp.addNumber(3);
+// sp.addNumber(17);
+// sp.addNumber(9);
+// sp.addNumber(11);
+// std::cout << sp.shortestSpan() << std::endl;
+// // std::cout << sp.longestSpan() << std::endl;
+// return 0;
+// }
