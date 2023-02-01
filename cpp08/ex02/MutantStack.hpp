0151ae223dcc29ebe9cb7475c5b8512b4100fea4
Thirza-Os <74403468+Thirza-Os@users.noreply.github.com>
2023-02-01 19:37:50 +0100

    yay

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,20 +6,55 @@
 #include <algorithm>
 #include <vector>
 #include <iterator>
+#include <stack>
 
 template <typename T>
-class MutantStack
+class MutantStack: public std::stack<T>
 {
-private:
-    typename T::
-
 public:
-    MutantStack();
-    MutantStack();
-    ~MutantStack();
-	MutantStack(const MutantStack& copy);
-	MutantStack& operator=(const MutantStack & other);
+    // MutantStack(){};
+    // ~MutantStack(){};
+	// MutantStack(const MutantStack& copy){};
+	// MutantStack& operator=(const MutantStack & other){};
+
+    typedef typename std::deque<T>::iterator iterator;
+    typedef typename std::deque<T>::iterator const_iterator;
+
+    iterator begin()
+    {
+        return this->c.begin();
+    }
+
+    iterator end()
+    {
+        return this->c.end();
+    }
+
+    const_iterator cbegin() const
+    {
+        return this->c.cbegin();
+    }
+
+    const_iterator cend() const
+    {
+        return this->c.cend();
+    }
 
 };
 
 #endif
+
+// The `begin()` and `end()` functions provide mutable iterators to the underlying container,
+// which means that you can modify the elements through these iterators.
+// On the other hand, the `cbegin()` and `cend()` functions provide constant iterators to the underlying container,
+// which means that you can only read the elements through these iterators and cannot modify them.
+
+// Having both mutable and constant iterators is a common pattern in C++
+// and it provides more flexibility in terms of how you use the iterators.
+// For example, you may want to use a constant iterator when you want to traverse the elements of the container without modifying them,
+// and use a mutable iterator when you want to modify the elements of the container.
+
+// In general, it is a good practice to provide both constant and mutable iterators for containers,
+// so that the user of the container can choose the appropriate type of iterator based on their needs.
+// However, if you only need to provide constant iterators,
+// then you only need to provide `cbegin()` and `cend()` functions.
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,7 +1,45 @@
 #include "MutantStack.hpp"
 
 #include <iostream>
+// int main()
+// {
+//     MutantStack<int> sp;
+//     sp.push(1);
+//     sp.push(2);
+
+//     for (std::deque<int>::iterator it = sp.begin(); it != sp.end(); ++it) {
+//     std::cout << *it << ' ';
+//     }
+
+//     int size = sp.size();
+
+
+//     std::cout << size << std::endl;
+
+// }
+
 int main()
 {
-MutantStack sp;
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
 }
